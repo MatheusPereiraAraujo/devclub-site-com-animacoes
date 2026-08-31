52a315240f88ed699951c32984d0249bdd0d0718
Matheus Pereira Araujo <matheuspereiraaraujo9@gmail.com>
2026-08-30 22:15:37 -0300

    Primeira versão do site DevClub Store

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 const prevButton = document.getElementById('prev');
 const nextButton = document.getElementById('next');
 const items = document.querySelectorAll('.item');
-const dots = document.querySelectorAll('.dots');
+const dots = document.querySelectorAll('.dot');
 const numberIndicator = document.querySelector('.numbers');
 const list = document.querySelector('.list');
 
@@ -13,8 +13,8 @@ let timer;
 
 function update(direction) {
 
-    document.querySelector('item.active').classList.remove('active');
-    document.querySelector('dot.active').classList.remove('active');
+    document.querySelector('.item.active').classList.remove('active');
+    document.querySelector('.dot.active').classList.remove('active');
 
 
     if(direction > 0){
@@ -32,11 +32,17 @@ function update(direction) {
         }
     }
 
-
     items[active].classList.add('active')
     dots[active].classList.add('active')
 
+    numberIndicator.textContent = String(active + 1).padStart(2, '0')
 }
+
+clearInterval(timer)
+timer = setInterval(() => {
+    update(1)
+}, 5000);
+
 
 
 
